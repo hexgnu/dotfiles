699dc7309f9e8218c64463031e23008781c0ae38
Matt Kirk <matt@matthewkirk.com>
2016-08-30 14:31:34 -0700

    Update using more mac specific stuffs

--- bash/prompt_command.c
+++ bash/prompt_command.c
@@ -1,19 +1,25 @@
 #include <sys/ioctl.h>
 #include <stdio.h>
-#include <sys/unistd.h>
+#include <unistd.h>
 #include <string.h>
 #include <stdlib.h>
 
 #define ANSI_COLOR_MAGENTA  "\x1b[1;35m"
 #define ANSI_COLOR_RESET    "\x1b[1;0m"
 #define ANSI_COLOR_CYAN     "\x1b[1;36m"
+#define ANSI_COLOR_GREY     "\x1b[0;37m"
+#define ANSI_COLOR_YELLOW   "\x1b[1;33m"
 int main() {
   struct winsize w;
   ioctl(STDOUT_FILENO, TIOCGWINSZ, &w);
+
   char hostname[128];
   char clean_hostname[128];
+
   char username[128];
+
   char buffer[1024];
+  char dashes[1024];
   int i;
 
   getlogin_r(username, sizeof username);
@@ -26,8 +32,7 @@ int main() {
     c++;
   }
 
-  char dashes[1024];
-  snprintf(buffer, sizeof(buffer), "--(" ANSI_COLOR_MAGENTA "%s" ANSI_COLOR_RESET "@" ANSI_COLOR_CYAN "%s" ANSI_COLOR_RESET ")-", username, clean_hostname);
+  snprintf(buffer, sizeof(buffer), ANSI_COLOR_GREY "--(" ANSI_COLOR_MAGENTA "%s" ANSI_COLOR_GREY "@" ANSI_COLOR_CYAN "%s" ANSI_COLOR_GREY ")-", username, clean_hostname);
 
 
   for (i = 0; i < w.ws_col; i++) {
@@ -38,7 +43,7 @@ int main() {
 
   strcat(buffer, dashes);
 
-  int offset = w.ws_col + 2* sizeof(ANSI_COLOR_RESET) + sizeof(ANSI_COLOR_MAGENTA) +  sizeof(ANSI_COLOR_CYAN)-4;
+  int offset = w.ws_col + 3*sizeof(ANSI_COLOR_GREY) + sizeof(ANSI_COLOR_MAGENTA) + sizeof(ANSI_COLOR_CYAN) - 5;
 
   buffer[offset] = '\0';
   printf(buffer);
